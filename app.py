34e49dc7bbb06d944b9a1d441f4c7170288a7f28
Arsystem808 <bwr0888@gmail.com>
2025-09-13 11:43:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def render_arxora_header():
     if os.path.exists(hero_path):
         st.image(hero_path, use_container_width=True)
     else:
-        PURPLE = "#673ab7"; BLACK = "#000000"
+        PURPLE = "#4746f8"; BLACK = "#000000"
         st.markdown(
             f"""
             <div style="border-radius:8px;overflow:hidden;
