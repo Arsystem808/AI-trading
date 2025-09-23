635543e8c094042318e928a4ffa79549208d994f
Arsystem808 <bwr0888@gmail.com>
2025-09-24 00:55:24 +0300

    Update prepare_and_train.py

--- scripts/prepare_and_train.py
+++ scripts/prepare_and_train.py
@@ -37,6 +37,8 @@ def train_and_save_model(ticker, df):
     X = df[feature_cols]
     y = df[target_col]
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+model = LGBMRegressor(n_estimators=1000, learning_rate=0.05)
+
 
     model = LGBMRegressor(n_estimators=1000, learning_rate=0.05)
     model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=20)
