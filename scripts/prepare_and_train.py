0c58062fbe6b71616da749b1be7e3a12286d2e78
Arsystem808 <bwr0888@gmail.com>
2025-09-24 00:26:01 +0300

    Update prepare_and_train.py

--- scripts/prepare_and_train.py
+++ scripts/prepare_and_train.py
@@ -39,7 +39,7 @@ def train_and_save_model(ticker, df):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
     model = LGBMRegressor(n_estimators=1000, learning_rate=0.05)
-    model.fit(X_train, y_train, eval_set=[(X_test, y_test)], early_stopping_rounds=50, verbose=20)
+    model.fit(X_train, y_train, eval_set=[(X_test, y_test)], verbose=20)
 
     os.makedirs("models", exist_ok=True)
     model_path = f"models/arxora_m7pro_{ticker}.joblib"
